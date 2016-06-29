314daebda2ec0f077d7a5294e56859a1270b12a3
Natercio <natercio.moniz@vilt-group.com>
2016-06-29 22:22:43 +0100

    added product

---
+++ src/main/java/com/natercio/Product.java
@@ -0,0 +1,17 @@
+package com.natercio;
+
+/**
+ * Created by natercio on 29/06/16.
+ */
+public class Product {
+
+    public final String name;
+
+    public final double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+}
