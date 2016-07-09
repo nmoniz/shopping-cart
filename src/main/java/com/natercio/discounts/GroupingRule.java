15b228b4c2dc7fe806e64be9f181907e9aa75f7f
natercio moniz <natercio.moniz@gmail.com>
2016-07-09 15:50:32 +0100

    add grouping rule abstract

---
+++ src/main/java/com/natercio/discounts/GroupingRule.java
@@ -0,0 +1,36 @@
+package com.natercio.discounts;
+
+import com.natercio.Product;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class GroupingRule implements Rule {
+
+    @Override
+    public List<Product> apply(List<Product> products) {
+        List<Product> candidates = candidates(products);
+
+        return products.stream()
+                .map(product -> {
+                    if (candidates.contains(product)) {
+                        return transform(product);
+                    } else {
+                        return product.clone();
+                    }
+                })
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * @param product Product to transform
+     * @return this should return a transformed product
+     */
+    protected abstract Product transform(Product product);
+
+    /**
+     * @param products list of products to search
+     * @return the list of products that should be transformed
+     */
+    protected abstract List<Product> candidates(List<Product> products);
+}
